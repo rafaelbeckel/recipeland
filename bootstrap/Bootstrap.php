f56e982fd4bbce0d7a998dd3f255fd8b52fca659
Rafael Beckel <rafaelbeckel@gmail.com>
2018-02-21 02:14:02 +0000

    Implements Whoops error handler - http://filp.github.io/whoops

--- bootstrap/Bootstrap.php
+++ bootstrap/Bootstrap.php
@@ -1,3 +1,23 @@
 <?php
 
+error_reporting(E_ALL);
+require __DIR__ . '/../vendor/autoload.php';
+
+$env = new Dotenv\Dotenv(__DIR__.'/..');
+$env->load();
+
+$handler = new Whoops\Run;
+
+if (getenv('ENVIRONMENT') == 'development') {
+    $handler->pushHandler(new Whoops\Handler\PrettyPageHandler);
+    
+} else {
+    $handler->pushHandler(function($error){
+        // @todo log the error and send alert to developers
+        echo "Some error message";
+    });
+    
+}
+$handler->register();
+
 echo "Hi!";
