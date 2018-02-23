13cdb661c8e89ab0629a46750a2c638ee8fb93e5
Rafael Beckel <rafaelbeckel@gmail.com>
2018-02-23 02:55:43 +0000

    Closes #3 - Creates the Router object and a Controller for handling the requests

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -14,15 +14,6 @@ abstract class Controller
         $this->response = new Response;
     }
     
-    /**
-     * Handle calls to missing methods on the controller.
-     *
-     * @param  string  $method
-     * @param  array   $parameters
-     * @return mixed
-     *
-     * @throws \BadMethodCallException
-     */
     public function __call($method, $parameters)
     {
         throw new BadMethodCallException("Method [$method] does not exist.");
@@ -31,11 +22,6 @@ abstract class Controller
     protected function render()
     {
         $this->response->send();
-    }
-    
-    static public function getNamespace()
-    {
-        return __NAMESPACE__;
     }
     
 }
