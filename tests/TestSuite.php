90aba253cfc5a719d7f37e4d858dab48347451fa
Rafael Beckel <rafaelbeckel@gmail.com>
2018-03-02 11:02:50 +0000

    Adds a custom experimental test formatter

---
+++ tests/TestSuite.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests;
+
+use \Mockery as m;
+use PHPUnit\Framework\TestCase;
+
+abstract class TestSuite extends TestCase
+{
+    
+    public static function setUpBeforeClass()
+    {
+        $bold = "\033[2m";
+        $normal = "\033[0m";
+        echo "\n".$bold.static::class.$normal.": \n";
+    }
+    
+    public function setUp()
+    {
+        echo " ";
+    }
+    
+    public function tearDown() 
+    {
+        m::close();
+        
+        if ($this->hasFailed()) {
+            $this->red( $this->getStatusMessage() );
+        } else {
+            $this->green("ok!");
+        }
+        echo "\n";
+    }
+    
+    private function red($text) {
+        $red = "\033[0;31m";
+        $white = "\033[0m";
+        echo ": ".$red.$text.$white;
+    }
+    
+    private function green($text) {
+        $green = "\033[0;32m";
+        $white = "\033[0m";
+        echo ": ".$green.$text.$white;
+    }
+}
