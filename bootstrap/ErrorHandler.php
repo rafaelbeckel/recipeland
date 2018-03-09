7bccaffcaa74539609a244e534189e6b8c93d43c
Rafael Beckel <rafaelbeckel@gmail.com>
2018-03-09 10:15:24 +0000

    Error handler: use the Logger to log errors if we are in production

--- bootstrap/ErrorHandler.php
+++ bootstrap/ErrorHandler.php
@@ -1,14 +1,25 @@
 <?php
+
+use Monolog\Logger;
 use Whoops\Run as ErrorHandler;
+use Monolog\Handler\StreamHandler;
 
 error_reporting(E_ALL);
+
 
 /**
  * Register the error handler for friendly error messages
  */
 $err = new ErrorHandler;
 
-if (getenv('ENVIRONMENT') == 'development') {
+$errorLog = new Logger('error');
+$errorLog->pushHandler(new StreamHandler(__DIR__.'/../log/error.log', Logger::WARNING));
+
+$accessLog = new Logger('access');
+$accessLog->pushHandler(new StreamHandler(__DIR__.'/../log/access.log', Logger::INFO));
+
+
+if (getenv('ENVIRONMENT') != 'production') {
     $err->pushHandler(new Whoops\Handler\PrettyPageHandler);
     
 } else {
@@ -17,18 +28,16 @@ if (getenv('ENVIRONMENT') == 'development') {
          * Generic error message for production environment
          */
         echo "
-        Swigert: \"Okay, Houston, we've had a problem here.\"       \n
-        Lousma:  \"This is Houston. Say again, please.\"            \n
-        Lovell:  \"Uh, Houston, we've had a problem.\"              \n
-        Lovell:  \"We've had a MAIN B BUS UNDERVOLT.\"              \n
-        Lousma:  \"Roger, MAIN B UNDERVOLT.\"                       \n
-        Lousma:  \"Okay, stand by, Thirteen, we're looking at it.\" \n
-        ";
+            Swigert: \"Okay, Houston, we've had a problem here.\"       \n
+            Lousma:  \"This is Houston. Say again, please.\"            \n
+            Lovell:  \"Uh, Houston, we've had a problem.\"              \n
+            Lovell:  \"We've had a MAIN B BUS UNDERVOLT.\"              \n
+            Lousma:  \"Roger, MAIN B UNDERVOLT.\"                       \n
+            Lousma:  \"Okay, stand by, Thirteen, we're looking at it.\" \n
+        "; // @TODO pull this message from config or lang class
         
-        // @TODO pull message from config or lang class
-        
-        // @TODO log the error and send email to developers
-        
+        $errorLog->error($e->getMessage());
+        $errorLog->debug($e->getTraceAsString());
     });
 }
 
