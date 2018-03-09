bf940254ac5895ee3c065395c4f73a64eb432010
Rafael Beckel <rafaelbeckel@gmail.com>
2018-03-09 10:24:26 +0000

    Add middleware to insert security headers. The same headers are set up in our Docker's nginx, but we can't count on it. This app may run in other environments that we do not control.

---
+++ src/Middleware/AddSecurityHeaders.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Recipeland\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface as RequestInterface;
+use Psr\Http\Server\RequestHandlerInterface as HandlerInterface;
+
+class AddSecurityHeaders implements MiddlewareInterface
+{
+    public function process(RequestInterface $request, HandlerInterface $next): ResponseInterface
+    {
+        $response = $next->handle($request);
+        
+        return $response->withHeader("Strict-Transport-Security", "\"max-age=31536000; includeSubDomains; preload\"")
+                        ->withHeader("Content-Security-Policy",   "\"default-src 'self';\""                         )
+                        ->withHeader("X-XSS-Protection",          "\"1; mode=block\""                               )
+                        ->withHeader("X-Frame-Options",           "\"DENY\""                                        )
+                        ->withHeader("X-Content-Type-Options",    "nosniff"                                         );
+    }
+}
