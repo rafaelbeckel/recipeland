90afe8e64fe8750c70adae48d9941bbbde932952
Rafael Beckel <rafaelbeckel@gmail.com>
2018-03-09 14:35:08 +0000

    All files with strict type mode enabled

--- src/Helpers/Validators/RoutesArrayValidator.php
+++ src/Helpers/Validators/RoutesArrayValidator.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Recipeland\Helpers\Validators;
 
@@ -7,51 +7,45 @@ use Recipeland\Helpers\Validator;
 
 class RoutesArrayValidator extends Validator
 {
-    //@TODO pull from lang file
-    const EMPTY_ARRAY = "Route collection array cannot be empty";
-    const ERROR_CONTROLLER_NOT_FOUND = "Error controller not found";
-    const INVALID_ELEMENT_COUNT = "Route array must have 3 elements";
-    const FIRST_ELEMENT_MUST_BE_REQUEST_METHOD = "First element of routes array must be Request Method ('GET', 'POST', etc.)";
-    const SECOND_ELEMENT_MUST_BE_URL_PATH = "Second element of routes array must be URL Path";
-    const THIRD_ELEMENT_MUST_BE_CONTROLLER_AND_ACTION = "Third element of routes array must be in the format Controller@action";
-    const URL_PATH_PATTERN = "|(\/)([\w\/\[\]\{\}]*)(\??[\w\/\[\]\{\}]+\=[\w\/\[\]\{\}]+)*(\&?[\w\/\[\]\{\}]+\=[\w\/\[\]\{\}]+)*|i";
-    const AT_PATTERN = "|[^@]*@[^@]*|";
-    
-    private $HTTP_Methods = [
-        'HEAD',
-        'GET',
-        'POST',
-        'PUT',
-        'PATCH',
-        'DELETE',
-        'PURGE',
-        'OPTIONS',
-        'TRACE',
-        'CONNECT',
-    ];
-    
+    private $HTTP_Methods = ['HEAD', 'GET', 'POST', 'PUT', 
+                             'PATCH', 'DELETE', 'OPTIONS', 
+                             'PURGE', 'TRACE', 'CONNECT'];
     
     public function validate($routes): void
     {
-        $this->validateRoutesArray($routes);
-    }
-    
-    private function validateRoutesArray(array $routes): void
-    {
-        if (empty($routes)) {
-            throw new InvalidArgumentException(self::EMPTY_ARRAY);
-        }
+        
+        $this->not_empty($routes);
+        
+        $this->must_be_array($routes);
+        
+        
+        
+        
+        
+        
+        
         
         foreach ($routes as $route) {
             $this->validateRoute($route);
         }
+        
     }
     
-    private function validateRoute(array $route): void
+    protected function not_empty($value)
     {
-        if (count($route) !== 3) {
-            throw new InvalidArgumentException(self::INVALID_ELEMENT_COUNT);
+        if (empty($value)) {
+            throw new InvalidArgumentException(self::EMPTY_ARRAY);
         }
+    }
+    
+    protected function validateRoute(array $route): void
+    {
+        
+        $this->is_between(count($route), 3, 4);
+        
+        // if (! $this->between(count($route), 3, 4)) {
+        //     throw new InvalidArgumentException(self::INVALID_ELEMENT_COUNT);
+        // }
         
         if (! in_array($route[0], $this->HTTP_Methods)) {
             throw new InvalidArgumentException(self::FIRST_ELEMENT_MUST_BE_REQUEST_METHOD);
